69ec55910d56c2f606cb625e0001b6e745e76bec
Bartek <bartekmackowiak92@gmail.com>
2025-01-08 16:48:40 +0100

    how to use callbacks

--- auth.ts
+++ auth.ts
@@ -1,9 +1,62 @@
-import NextAuth from 'next-auth';
+import NextAuth, { DefaultSession } from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import authConfig from './auth.config';
 import { db } from './lib/db';
+import { getUserById } from './data/user';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { JWT } from 'next-auth/jwt';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      role: 'ADMIN' | 'USER';
+      username: string | null;
+    } & DefaultSession['user'];
+  }
+}
+
+declare module 'next-auth/jwt' {
+  /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
+  interface JWT {
+    /** OpenID ID Token */
+    role: 'ADMIN' | 'USER';
+    username: string | null;
+  }
+}
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    // async signIn({ user }) {
+    //   const existingUser = await getUserById(user.id as string);
+
+    //   if (!existingUser || !existingUser.emailVerified) {
+    //     return false;
+    //   }
+
+    //   return true;
+    // },
+    async session({ token, session }) {
+      console.log(token);
+
+      if (session.user && token.sub) {
+        session.user.id = token.sub;
+        session.user.username = token.username;
+        session.user.role = token.role;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+      token.username = existingUser.username;
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,
